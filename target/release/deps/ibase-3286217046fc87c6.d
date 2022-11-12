a87659db70cd77519382a8144ccc52976cab8196
lucas-almeida026 <vcxzop1@gmail.com>
2022-11-12 18:39:57 -0300

    add blobs

---
+++ target/release/deps/ibase-3286217046fc87c6.d
@@ -0,0 +1,5 @@
+/home/almeida/Desktop/rust/ibase/target/release/deps/ibase-3286217046fc87c6: src/main.rs
+
+/home/almeida/Desktop/rust/ibase/target/release/deps/ibase-3286217046fc87c6.d: src/main.rs
+
+src/main.rs:
---
+++ target/x86_64-pc-windows-gnu/release/ibase.d
@@ -0,0 +1,1 @@
+/home/almeida/Desktop/rust/ibase/target/x86_64-pc-windows-gnu/release/ibase.exe: /home/almeida/Desktop/rust/ibase/src/main.rs
